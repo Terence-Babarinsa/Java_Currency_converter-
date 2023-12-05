8f0c5244482a2ae803301009f277a72bbd1dfcbd
Terence-Babarinsa <69308628+Terence-Babarinsa@users.noreply.github.com>
2023-12-05 15:27:11 +0000

    Unfinished project

---
+++ java_currecny_converter/src/java_currecny_converter/Converter_json.java
@@ -0,0 +1,42 @@
+package java_currecny_converter;
+
+
+public class Converter_json {
+private String result;
+private String provider;
+private String documentation;
+private String terms_of_use;
+private int time_last_update_unix;
+private String time_last_update_utc;
+private int time_next_update_unix;
+private String time_next_update_utc;
+private int time_eol_unix;
+private String base_code;
+
+ /*class Rates() 
+{
+		
+	private String country;
+	private int conv_rate;
+	
+	public Rates() {}
+	
+	@JsonProperty("country")
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	@JsonProperty("conv_rate")
+	public int getConvRate() {
+		return conv_rate;
+	}
+	
+	public void setConvRate(int conv_rate) {
+		this.conv_rate = conv_rate;
+	}*/
+}
+
---
+++ java_currecny_converter/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module java_currecny_converter {
+}
